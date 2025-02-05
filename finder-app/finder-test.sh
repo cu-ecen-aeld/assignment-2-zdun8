79488c26e22f58c1dde44aa15aafe8a1771c25f3
zdun <zdun@ubuntu.myguest.virtualbox.org>
2025-02-05 01:05:53 +0100

    Cleanup

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -55,7 +55,6 @@ file writer > ../fileresult.txt
 
 for i in $( seq 1 $NUMFILES)
 do
-	#./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
